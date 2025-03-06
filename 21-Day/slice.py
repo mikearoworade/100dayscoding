1bb962fd1160750ad69d3d0b6138a0bf6dee65e3
Michael Aroworade <Ayoaro85@gmail.com>
2025-03-06 18:51:05 +0100

    Snake and pong game completed

---
+++ 21-Day/slice.py
@@ -0,0 +1,9 @@
+# Slicing
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+
+print(piano_keys[2:5]) # Print index 2 to 5
+print(piano_keys[2:]) # print index 2 to end
+print(piano_keys[:5]) # print beginning to index 5
+print(piano_keys[2:5:2]) # print index 2 to 5 but count by 2 (second count included)
+print(piano_keys[::2]) # Skip by 2
+print(piano_keys[::-1]) # Reverse List
